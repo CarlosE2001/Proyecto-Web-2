39ac9f005254e54702b04c121494ea6f96c1a942
Carlos Espinoza <carlos.espinozaperaza@ucr.ac.cr>
2021-11-10 21:03:18 -0600

    Initial commit of the branch

--- ProyectoMVC/App_Start/BundleConfig.cs
+++ ProyectoMVC/App_Start/BundleConfig.cs
@@ -17,10 +17,10 @@ namespace ProyectoMVC {
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.bundle.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap.min.css",
                       "~/Content/site.css"));
         }
     }
